a25f64be4edb97b8bf2c91683b21891e5a510c89
yeohyun Yoon <iob19971013@gmail.com>
2023-01-04 09:41:15 +0900

    initial commit with ui

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 2,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 88,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 5,
+            child: Center(
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.play_circle_outline_rounded,
+                ),
+                iconSize: 110,
+                color: Theme.of(context).cardColor,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration:
+                        BoxDecoration(color: Theme.of(context).cardColor),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Routines',
+                          style: TextStyle(
+                            color: Theme.of(context).textTheme.headline1!.color,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            color: Theme.of(context).textTheme.headline1!.color,
+                            fontSize: 40,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
